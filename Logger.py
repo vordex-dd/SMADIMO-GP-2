021a02275ee4ab97fa591a880cf5f216cd8238c7
Delariu Dmitry <107302386+vordex-dd@users.noreply.github.com>
2025-02-24 23:23:12 +0300

    Create pylint.yml

--- Logger.py
+++
@@ -1,8 +0,0 @@
-import logging
-import logging.config
-
-
-class Logger:
-    @staticmethod
-    def set_up_logger():
-        logging.config.fileConfig('logging.conf')
---
+++ logger.py
@@ -0,0 +1,8 @@
+import logging
+import logging.config
+
+
+class LoggerSettings:
+    @staticmethod
+    def set_up():
+        logging.config.fileConfig('logging.conf')
