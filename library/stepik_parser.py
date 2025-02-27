0aa1d49e8202f2ea95d55e2f3ad49d6243c9337f
Delariu Dmitry <Ddelariu@ya.ru>
2025-02-27 17:57:02 +0300

    Add logging in stepik parser and update readme

--- library/stepik_parser.py
+++ library/stepik_parser.py
@@ -1,6 +1,8 @@
 import json
+import logging
 import os
 import requests
+from logger import LoggerSettings
 
 
 class StepikParser:
@@ -8,32 +10,35 @@ class StepikParser:
     def __init__(self):
         self.base_path = 'https://stepik.org/api/courses?page='
 
-
     def get_data(self):
+        logging.info('Start fetching Stepik API')
         current_page = 1
         while True:
             url = f'{self.base_path}{current_page}'
             current_courses, has_next = self.fetch_url(url)
-
+            logging.debug(f'Finish fetching {current_page} page')
             yield current_courses
 
             if has_next:
                 current_page += 1
             else:
+                logging.info('Finish fetching Stepik API')
                 break
-
 
     @staticmethod
     def fetch_url(url: str):
         try:
-            response = requests.get(url, timeout = 120).json()
+            response = requests.get(url, timeout=120).json()
             return response['courses'], response['meta']['has_next']
-        except ValueError:
+        except ValueError as error:
+            logging.error(error)
             return [], True
 
 
 if __name__ == '__main__':
-    FOLDER_PATH = '../parsed_data'
+    LoggerSettings.set_up()
+
+    FOLDER_PATH = './../parsed_data'
     FILE_PATH = os.path.join(FOLDER_PATH, 'stepik_courses.json')
 
     if not os.path.exists(FOLDER_PATH):
@@ -43,16 +48,8 @@ if __name__ == '__main__':
             pass
 
     parser = StepikParser()
-    result = []
-    CNT = 0
 
     with open(FILE_PATH, 'a', encoding='utf-8') as file:
         for courses in parser.get_data():
             for course in courses:
                 file.write(json.dumps(course, ensure_ascii=False) + '\n')
-            result.extend(courses)
-            CNT += 1
-
-            if CNT % 100 == 0:
-                print(f'i: {CNT}, courses length: {len(result)}')
-                print()
