1785016932c077b1e088fa45f4bd0ecdfbf5f23d
Maxim <113048144+mxlq1@users.noreply.github.com>
2025-02-28 18:05:20 +0300

    Fixed pep8 in check_directory

--- library/check_directory.py
+++ library/check_directory.py
@@ -2,13 +2,13 @@ import os
 
 
 def check_directory(file_name):
-    FOLDER_PATH = './../parsed_data'
-    FILE_PATH = os.path.join(FOLDER_PATH, file_name)
+    folder_path = './../parsed_data'
+    file_path = os.path.join(folder_path, file_name)
 
-    if not os.path.exists(FOLDER_PATH):
-        os.makedirs(FOLDER_PATH)
-    if not os.path.exists(FILE_PATH):
-        with open(FILE_PATH, 'w', encoding='utf-8'):
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    if not os.path.exists(file_path):
+        with open(file_path, 'w', encoding='utf-8'):
             pass
 
-    return FILE_PATH
+    return file_path
