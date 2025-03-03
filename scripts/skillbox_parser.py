b4fedf7f182e8dea45ad89b2296f34a26281f4e9
sdmatylev <nszvdl.matylev@yandex.ru>
2025-03-04 01:19:04 +0300

    Fix PEP8

--- scripts/skillbox_parser.py
+++ scripts/skillbox_parser.py
@@ -4,7 +4,7 @@ import logging
 from check_directory import check_directory
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.common import NoSuchElementException, TimeoutException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
@@ -16,15 +16,19 @@ class SkillboxParser:
         self.driver = webdriver.Safari()
         self.url = 'https://skillbox.ru/courses/'
 
-    def get_data(self):
+    def fetch_page_source(self):
         logging.info('Start fetching Skillbox')
         try:
             self.driver.get(self.url)
         except ValueError as error:
             logging.error('Failed to scrap data from Skillbox')
             logging.error(error)
-            return []
+            self.driver.quit()
+            return None
 
+        return self.expand_courses_list()
+
+    def expand_courses_list(self):
         logging.info('Start expanding the Skillbox courses list')
         more_button_xpath = '//*[@id="#app"]/main/div[1]/div[2]/div/div[2]/div/section[2]/button'
         button_press_counter = 0
@@ -45,16 +49,22 @@ class SkillboxParser:
                 logging.info('Finish expanding the Skillbox courses list')
                 html_content = self.driver.page_source
                 self.driver.quit()
-                break
+                return html_content
 
+    def extract_courses(self, html_content):
         soup = BeautifulSoup(html_content, 'lxml')
         courses_container = soup.find_all('div', class_='card-list courses-block__list card-list--catalog')[1]
+
         courses = courses_container.find_all('article', class_='ui-product-card')
         if len(courses) > 0:
             logging.info(f'Fetched {len(courses)} courses from Skillbox')
         else:
             logging.error('Failed to scrap data from Skillbox')
 
+        return self.parse_courses(courses)
+
+    @staticmethod
+    def parse_courses(courses):
         courses_data = []
         for i, course in enumerate(courses):
             logging.debug(f'Start fetching {i} course')
@@ -63,9 +73,11 @@ class SkillboxParser:
             if title:
                 title = title.text.replace('\u2028', '').replace('\u00ad', '').strip()
 
-            duration_amount, duration_units = (course.find('span', class_='card__duration f f--12')
-                                               .text.replace('\n', '')
-                                               .strip().split())
+            duration_amount, duration_units = (
+                course.find('span', class_='card__duration f f--12')
+                .text.replace('\n', '')
+                .strip().split()
+            )
 
             is_popular = course.find('span', string='Популярное')
             if is_popular:
@@ -84,6 +96,12 @@ class SkillboxParser:
             logging.debug(f'Finish fetching {i} course')
 
         return courses_data
+
+    def get_data(self):
+        html_content = self.fetch_page_source()
+        if html_content is None:
+            return []
+        return self.extract_courses(html_content)
 
 
 if __name__ == '__main__':
